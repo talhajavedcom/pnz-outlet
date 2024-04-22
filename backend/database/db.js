d786deaddf8dd56eead86dcdcd44b1e8a7b7710d
Talha Javed <98523646+talhajaved-com@users.noreply.github.com>
2024-04-22 09:09:57 +0500

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://merntalhajaved:merntalhajaved@cluster0.41feiz2.mongodb.net/ecommerce');
+    const conn = await mongoose.connect('');
     console.log(`Connnected to MongoDB ${conn.connection.host}`.bgGreen.yellow);
   } catch (error) {
     console.log(`Error in DB ${error}`.bgRed.white);
